6209e8825ff91b536aaea263f0b66609d4ef55ef
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-03-15 12:01:44 -0300

    -- Home Responsive --

--- src/components/PokemonMapCards.jsx
+++ src/components/PokemonMapCards.jsx
@@ -1,11 +1,14 @@
 import useGetImage from "../customHooks/useGetImage";
+import styles from "./PokemonMapCards.module.css";
 const PokemonMapCards = ({ pokemon }) => {
   const { name, url } = pokemon;
   const [image] = useGetImage(url);
   return (
-    <div>
-      <h2>{name}</h2>
-      <img src={image} alt={`Imagen de ${name}`} />
+    <div className={styles.pokemonCard}>
+      <div className={styles.pokeContainer}>
+        <img src={image} alt={`Imagen de ${name}`} />
+        <h2>{name}</h2>
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,21 @@
 import { Link, useLoaderData } from "react-router-dom";
 import PokemonMapCards from "../components/PokemonMapCards";
-
+import Logo from "../assets/logo.png";
+import styles from "./Home.module.css";
 const Home = () => {
   const pokemons = useLoaderData();
   return (
     <>
-      {pokemons.map((pokemon) => (
-        <Link to={`/${pokemon.name}`} key={pokemon.name} state={pokemon}>
-          <PokemonMapCards pokemon={pokemon} />
-        </Link>
-      ))}
+      <header className={styles.header}>
+        <img src={Logo} className={styles.logo} alt="Logotipo de Pokemon" />
+      </header>
+      <main>
+        {pokemons.map((pokemon) => (
+          <Link to={`/${pokemon.name}`} key={pokemon.name} state={pokemon}>
+            <PokemonMapCards pokemon={pokemon} />
+          </Link>
+        ))}
+      </main>
     </>
   );
 };
