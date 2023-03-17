ea84f29d49b5e7be51cd8cb8c7f4e9690c987772
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-03-17 12:07:43 -0300

    -- stats --

--- src/components/PokemonMapCards.jsx
+++ src/components/PokemonMapCards.jsx
@@ -6,22 +6,22 @@ const PokemonMapCards = ({ pokemon }) => {
   const [statsInfo, setStatsInfo] = useState({});
   const [image] = useGetImage(url);
   const { stats } = image;
+
   useEffect(() => {
     if (stats == undefined) return;
     const statsInformacion = stats.map((stat) => stat.base_stat);
     setStatsInfo(statsInformacion);
   }, [stats]);
-
   return (
     <div className={styles.pokemonCard}>
       <div className={styles.pokeContainer}>
         <img src={image.sprites?.front_default} alt={`Imagen de ${name}`} />
         <h2>{name}</h2>
         <p>{image.weight}</p>
-        <p>{image.height}</p>
-        <p>{statsInfo[0]}</p>
-        <p>{statsInfo[1]}</p>
-        <p>{statsInfo[2]}</p>
+        <p>0{image.height}</p>
+        <p>0{statsInfo[0]}</p>
+        <p>0{statsInfo[1]}</p>
+        <p>0{statsInfo[2]}</p>
       </div>
     </div>
   );
