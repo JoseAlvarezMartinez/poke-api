071951ffdf51c8f732ca2bf68a4e48da643e5699
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-03-16 23:20:28 -0300

    -- Weight & Height --

--- src/components/PokemonMapCards.jsx
+++ src/components/PokemonMapCards.jsx
@@ -1,13 +1,17 @@
+import { useEffect, useState } from "react";
 import useGetImage from "../customHooks/useGetImage";
 import styles from "./PokemonMapCards.module.css";
 const PokemonMapCards = ({ pokemon }) => {
   let { name, url } = pokemon;
+  const [moves, setMoves] = useState("");
   const [image] = useGetImage(url);
   return (
     <div className={styles.pokemonCard}>
       <div className={styles.pokeContainer}>
         <img src={image.sprites?.front_default} alt={`Imagen de ${name}`} />
         <h2>{name}</h2>
+        <p>{image.weight}</p>
+        <p>{image.height}</p>
       </div>
     </div>
   );
--- src/customHooks/useGetImage.jsx
+++ src/customHooks/useGetImage.jsx
@@ -1,7 +1,7 @@
 import { useState,useEffect } from "react";
 import { consumirImg } from "../helpers/pokemons";
 const useGetImage = (url) => {
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState({});
   const getImages = async () => {
     const newImages = await consumirImg(url);
     setImage(newImages);
